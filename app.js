4c4744a7d92b5355f445eb9a83d911b65ab0e98b
Noah Kirschbaum <noah.kirschbaum@gmail.com>
2018-04-02 12:27:44 -0700

    Added Win Conditions

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 window.onload = function() {
 	var turn = 0;			//reset will make this 0
+	var gameOver = false;
+
+	var isValidMove = function() {
+		if (gameOver) {
+			alert("The game is over, hit reset to play again!");
+		}
+	}
 
 	var displayMark = function() {
 		if (turn % 2 === 0) {
@@ -7,26 +14,70 @@ window.onload = function() {
 		} else {
 			document.getElementById(this.id).innerHTML = "[O]";
 		}
-		turn++;
 		displayWinner();
+		turn++;
 	}
 
 	var resetGame = function() {
-		console.log('clicked');
 		turn = 0;
+		gameOver = false;
+		var elements = document.getElementsByTagName('span');
+		for (var i = 0; i < elements.length; i++) {
+			elements[i].innerHTML = '[ ]';
+		}
+		document.getElementById('status').innerHTML = "X Goes First";
+	}
+
+	var getBoardState = function() {
+		var elements = document.getElementsByTagName('span');
+		var boardState = [];
+
+		for (var i = 0; i < elements.length; i++) {
+			boardState.push(elements[i].childNodes[0].nodeValue.slice(1,2));
+		}	
+		return boardState;
 	}
 
 	var displayWinner = function() {
-		if (false) {
+		var board = getBoardState();
+		var test;
 
-		} else if (false) {
+		if (turn % 2 === 0) {
+			test = 'X';
+		} else {
+			test = 'O';
+		}
 
+		if (board[0] === test && board[1] === test && board[2] === test) {
+			document.getElementById('status').innerHTML = (test + " Wins!");
+			gameOver = true;
+		} else if (board[3] === test && board[4] === test && board[5] === test) {
+			document.getElementById('status').innerHTML = (test + " Wins!");
+			gameOver = true;
+		} else if (board[6] === test && board[7] === test && board[8] === test) {
+			document.getElementById('status').innerHTML = (test + " Wins!");
+			gameOver = true;
+		} else if (board[0] === test && board[3] === test && board[6] === test) {
+			document.getElementById('status').innerHTML = (test + " Wins!");
+			gameOver = true;
+		} else if (board[1] === test && board[4] === test && board[7] === test) {
+			document.getElementById('status').innerHTML = (test + " Wins!");
+			gameOver = true;
+		} else if (board[2] === test && board[5] === test && board[8] === test) {
+			document.getElementById('status').innerHTML = (test + " Wins!");
+			gameOver = true;
+		} else if (board[0] === test && board[4] === test && board[8] === test) {
+			document.getElementById('status').innerHTML = (test + " Wins!");
+			gameOver = true;
+		} else if (board[2] === test && board[4] === test && board[6] === test) {
+			document.getElementById('status').innerHTML = (test + " Wins!");
+			gameOver = true;
 		} else {
 			//Display whose turn it is
 			if (turn % 2 === 0) {
-				document.getElementById('turn').innerHTML = "X's Turn";
+				document.getElementById('status').innerHTML = "X's Turn";
 			} else {
-				document.getElementById('turn').innerHTML = "O's Turn";
+				document.getElementById('status').innerHTML = "O's Turn";
 			}
 		}
 	}
@@ -50,4 +101,7 @@ window.onload = function() {
 
 
 	//Edge Case: Prevent people from clicking something that has already been changed
+	//No more moves after game is done
+	//display ties
+
 }
